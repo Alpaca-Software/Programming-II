b33dfdb23fedfe3e89619e5925568e5c042fb246
alizarazot <alizarazot@ufpso.edu.co>
2023-08-17 13:22:16 -0500

    Add Exercise 4

---
+++ Exercises/Exercise4.java
@@ -0,0 +1,41 @@
+
+// Topic: ARITHMETIC OPERATORS - Operadores Aritméticos.
+
+// Exercise 4: An employee receives his monthly 
+//             salary in Colombian currency, if 
+//             he receives more than one minimum
+//             wage, add a transport bonus.
+//
+//             Un empleado recibe su salario mensual
+//             en moneda colombiana, si recibe más
+//             de un salario mínimo, añade una prima
+//             de transporte.
+
+// TODO: No creo que esto pase por «Clean Code»...
+
+public class Salary {
+  
+  // Constants.
+  static final float MINIMUM_WAGE = 1_300_606;
+  static final float TRANSPORT_BONUS = 140_606;
+  
+  public static void main(String[] args) {
+    // Inputs.
+    //float salary = 2_845_000;
+    float salary = 875_000;
+    
+    System.out.printf("Salario Mínimo: $%.0f\n", MINIMUM_WAGE);
+    System.out.printf("Prima de Transporte: $%.0f\n", TRANSPORT_BONUS);
+    
+    System.out.printf("\nSalario del Trabajador: $%.0f\n", salary);
+    System.out.printf("Prima de Transporte: %s.\n",
+      salary > MINIMUM_WAGE ? "Sí": "No"
+    );
+    
+    System.out.printf("\nTotal a Pagar: $%.0f\n",
+      salary > MINIMUM_WAGE ?
+        salary + TRANSPORT_BONUS:
+          salary
+    );
+  }
+}
