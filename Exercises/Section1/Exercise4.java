eb74ef34e3427f8858f6afe9c5e16845a5b272d2
alizarazot <alizarazot@ufpso.edu.co>
2023-08-18 08:35:06 -0500

    Correct some redaction errors

--- Exercises/Section1/Exercise4.java
+++ Exercises/Section1/Exercise4.java
@@ -3,11 +3,11 @@
 
 // Exercise 4: An employee receives his monthly 
 //             salary in Colombian currency, if 
-//             he receives more than one minimum
+//             he receives less than one minimum
 //             wage, add a transport bonus.
 //
 //             Un empleado recibe su salario mensual
-//             en moneda colombiana, si recibe más
+//             en moneda colombiana, si recibe menos
 //             de un salario mínimo, añade una prima
 //             de transporte.
 
@@ -27,13 +27,14 @@ public class Salary {
     System.out.printf("Salario Mínimo: $%.0f\n", MINIMUM_WAGE);
     System.out.printf("Prima de Transporte: $%.0f\n", TRANSPORT_BONUS);
     
+    // TODO: Maybe it can be refactored in a better way?
     System.out.printf("\nSalario del Trabajador: $%.0f\n", salary);
     System.out.printf("Prima de Transporte: %s.\n",
-      salary > MINIMUM_WAGE ? "Sí": "No"
+      salary < MINIMUM_WAGE ? "Sí": "No"
     );
     
     System.out.printf("\nTotal a Pagar: $%.0f\n",
-      salary > MINIMUM_WAGE ?
+      salary < MINIMUM_WAGE ?
         salary + TRANSPORT_BONUS:
           salary
     );
