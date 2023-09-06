f8fb1849528ad6da330fe9e121e0ba5d375b5fc3
pintofelipe <afpintou@ufpso.edu.co>
2023-09-06 09:35:20 -0500

    add sample class

---
+++ OOP/main/Main.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.main;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      
+        System.out.print("Input a digit: ");
+      int number = sc.nextInt();
+      // constructor = recibe datos de afuera
+      
+        Number numberOne = new Number(number);
+        
+        numberOne.allowDigit();
+        numberOne.showName();
+    }
+}
