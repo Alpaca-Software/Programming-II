1f7b91abd429b9351331f8cac4824bd13520a3ec
Felipepinto <afpintou@ufpso.edu.co>
2023-09-07 21:14:46 -0500

    add Exercice 2 OPP

---
+++ OOP/Exercise 2/Main2.java
@@ -0,0 +1,26 @@
+
+package com.mycompany.main2;
+
+/**
+ 2. Solicite dos números por teclado y muestre el menor de los dos
+ 2. Request two numbers by keyboard, and show the lesser of the two
+
+ */
+import java.util.*;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the number One: ");
+        int numberOne = sc.nextInt();
+        
+        System.out.print("Enter the number Two: ");
+        int numberTwo = sc.nextInt();
+        
+        Menor lesser = new Menor(numberOne,numberTwo);
+        
+        lesser.showLesser();
+    }
+}
---
+++ OOP/Exercise 2/Menor.java
@@ -0,0 +1,21 @@
+
+package com.mycompany.main2;
+
+
+public class Menor {
+    public int numberOne,numberTwo;
+
+    public Menor(int numberOne, int numberTwo) {
+        this.numberOne = numberOne;
+        this.numberTwo = numberTwo;
+    }
+
+    public void showLesser(){
+        if(numberOne<numberTwo){
+            System.out.print("The number lesser is: "+numberOne);
+        }else{
+            System.out.println("The number lesser is: "+numberTwo);
+        }
+    }
+    
+}
