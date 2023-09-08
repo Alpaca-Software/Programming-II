e5391093ae2ebb9845ed39d7cb3697a3e53830e5
Felipepinto <afpintou@ufpso.edu.co>
2023-09-07 21:55:58 -0500

    add Exercice 3 OPP

---
+++ OOP/Exercise3/Container.java
@@ -0,0 +1,25 @@
+
+package com.mycompany.metodo3;
+
+public class Container {
+    public String text,word;
+
+    public Container(String text, String word) {
+        this.text = text.toLowerCase();
+        this.word = word.toLowerCase();
+    }
+    
+    public void veryText(){
+        boolean e = true;
+     
+        e =  text.contains(word);
+        
+        if(e){
+            System.out.println("if exists the word!! ");
+        }else{
+            System.out.println("not exists tha word!!");
+        }
+        
+        
+    }
+}
