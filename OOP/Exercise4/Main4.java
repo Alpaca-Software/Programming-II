5f453e8bb9f66fb0573c8cb9227a57d0cf657280
Felipepinto <afpintou@ufpso.edu.co>
2023-09-07 22:43:36 -0500

    add Exercice 4 OPP

---
+++ OOP/Exercise4/Main4.java
@@ -0,0 +1,24 @@
+package com.mycompany.main4;
+
+/**
+ * 4. Solicitar y almacenar un número por teclado y generar un número con la
+ * función aleatoria, verificar en cuantos ciclos coinciden, el número
+ * solicitado no debe ser mayor a 20 ni menor o igual a cero.
+ */
+import java.util.*;
+
+public class Main4 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        
+        Random random = new Random(number);
+        
+        random.veryRandom();
+        
+       }
+}
