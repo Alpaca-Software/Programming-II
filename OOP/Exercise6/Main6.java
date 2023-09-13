736305e76206bbfaf6e586aecf9b6963b860aab4
Felipepinto <afpintou@ufpso.edu.co>
2023-09-12 20:37:18 -0500

    add Exercise 6 OPP

---
+++ OOP/Exercise6/Main6.java
@@ -0,0 +1,22 @@
+/*
+"6. Desarrollar una aplicación que calcule el área y perímetro de un lote rectangular, las dimensiones son propuestas por usted,
+       recuerda que debes crear métodos que realicen el cálculo"
+ */
+import java.util.*;
+public class Main6 {
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       
+        System.out.print("Enter the base: ");
+        float base = sc.nextInt();
+        System.out.print("Enter the height: ");
+        float height = sc.nextInt();
+        
+        Area area = new Area(base,height);
+        area.calculateArea();
+        String name = area.calculatePerimeter();
+        
+        System.out.println(name);
+    }
+}
