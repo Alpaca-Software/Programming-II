b87fe830131617657ee6d9ae0aa13a3bf3bed00e
pintofelipe <afpintou@ufpso.edu.co>
2023-09-18 09:15:38 -0500

    add folder the PartialOne and add exercise One OPP

---
+++ PartialOne/ExerciseOne/Worker.java
@@ -0,0 +1,41 @@
+
+package coco77.parcial;
+
+/**
+ */
+public class Worker {
+    
+    static final float payDay = 38667;
+    String workers[];    
+    String documet;
+    String name;
+    int workedDay;
+
+    public Worker(String[] workers, String documet, String name, int workedDay) {
+        this.workers = workers;
+        this.documet = documet;
+        this.name = name;
+        this.workedDay = workedDay;
+    }
+    
+    
+    public void salaryPayment(String[] workes){
+        int c = workes.length;
+        float payWorked = 0;
+        
+        for (int i = 0; i < c; i++) {
+            payWorked = workedDay*Worker.payDay;
+            System.out.println("the total value of the payroll a "+workes[i]+" is the $: "+payWorked);
+        }
+    }
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+}
