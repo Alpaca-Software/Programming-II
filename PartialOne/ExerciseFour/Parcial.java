048deccd5d31e640eb70a163deeaa2e3fd996f74
pintofelipe <afpintou@ufpso.edu.co>
2023-09-18 12:50:10 -0500

    add Exercise four partial One

---
+++ PartialOne/ExerciseFour/Parcial.java
@@ -0,0 +1,24 @@
+
+
+package coco77.parcial;
+
+/**
+"4. You have a list of students with the following data: name, 
+* score of the first partial, score of the second 
+    partial, total score of the quizes and the score of the final exam. 
+    * It is necessary to know if the student passes the subject."
+ */
+public class Parcial {
+
+    public static void main(String[] args) {
+        String name = "Felipe Pinto";
+        float scoreFirstPartial = (float) 3.0;
+        float scoreSecondPartial = (float) 3.0;
+        float quiz = (float) 3.0;
+        float finalExamen = (float) 3.0;
+        
+        Student student = new Student( name, scoreFirstPartial,  scoreSecondPartial, quiz, finalExamen);
+        
+        student.CalculateDefinive();
+    }
+}
---
+++ PartialOne/ExerciseFour/Student.java
@@ -0,0 +1,51 @@
+package coco77.parcial;
+
+/**
+ */
+public class Student {
+
+    String name;
+    float scoreFirstPartial;
+    float scoreSecondPartial;
+    float quiz;
+    float finalExamen;
+
+    public Student(String name, float scoreFirstPartial, float scoreSecondPartial, float quiz, float finalExamen) {
+        this.name = name;
+        this.scoreFirstPartial = scoreFirstPartial;
+        this.scoreSecondPartial = scoreSecondPartial;
+        this.quiz = quiz;
+        this.finalExamen = finalExamen;
+    }
+
+    public float CalculateSeventyNote() {
+        float seventyNote = 0;
+        
+        seventyNote = (this.scoreFirstPartial+this.scoreSecondPartial+this.quiz)/3;
+        
+        seventyNote = (seventyNote*70)/100;
+        
+        return seventyNote;
+    }
+
+    public float CalculateThirtyNote() {
+        float thirtyNote = 0;
+        
+        thirtyNote = (this.finalExamen*30)/100;
+        
+        return thirtyNote;
+    }
+    
+    public void CalculateDefinive() {
+        float finalNote = 0;
+        finalNote = CalculateSeventyNote()+CalculateThirtyNote();
+        
+        if(finalNote>=3.0){
+            System.out.println("The student passe the subject"+" note: "+finalNote);
+        }else{
+            System.out.println("The student no passe the subject");
+        }
+            
+    }
+
+}
