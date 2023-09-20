ce7b7fd98846a425231705ab6526e57f822c2a9e
pintofelipe <afpintou@ufpso.edu.co>
2023-09-20 09:53:12 -0500

    add exercise clase pillares

---
+++ ExerciseClass/Pillares/coco77/javaapplicationtest/JavaApplicationTest.java
@@ -0,0 +1,34 @@
+
+package coco77.javaapplicationtest;
+
+import newpackage.Customer;
+
+/**
+ */
+public class JavaApplicationTest {
+
+    public static void main(String[] args) {
+        
+        Customer customer1 = new Customer( "1049019557",  "Felipe",  200000);
+        
+        
+        
+        /*
+        customer1.document = "1049019557";
+        customer1.name = "Felipe";
+        customer1.save = 100000;*/
+        
+        
+        //customer1.setDocument("1049019557");
+        
+        //customer1.setName("Felipe");
+       // customer1.setSave(400000);
+        
+        
+        
+        System.out.println(customer1.getSave());
+
+        
+        
+    }
+}
