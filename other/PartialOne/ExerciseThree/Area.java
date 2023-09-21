dbbe4584cf88c99a0be1748074b785c247b6ed05
pintofelipe <afpintou@ufpso.edu.co>
2023-09-20 22:07:17 -0500

    add Exercise Three partial One

---
+++ other/PartialOne/ExerciseThree/Area.java
@@ -0,0 +1,48 @@
+package coco77.partialthree;
+
+/**
+ *
+ * @author pinto
+ */
+public class Area {
+    float baseTriangle;
+    float heightTriangle;
+    float baseRectangle;
+    float height;
+    float sideSquare;
+
+    public Area(float baseTriangle, float heightTriangle, float baseRectangle, float height, float sideSquare) {
+        this.baseTriangle = baseTriangle;
+        this.heightTriangle = heightTriangle;
+        this.baseRectangle = baseRectangle;
+        this.height = height;
+        this.sideSquare = sideSquare;
+    }
+    
+    public float calculateAreaTriangle(){
+        float areaTriangle = (this.heightTriangle*this.baseTriangle)/2;
+        System.out.println("areaTriangle = " + areaTriangle);
+        return areaTriangle;
+    }
+    
+    
+    public float calculateAreaRectangle(){
+        float areaRectangule = this.baseRectangle*this.height;
+        System.out.println("areaRectangule = " + areaRectangule);
+        return areaRectangule;
+        
+    }
+    
+    public float calculateAreaSquare(){
+        float areaSquare = (float) Math.pow(this.sideSquare, 2);
+        System.out.println("areaSquare = " + areaSquare);
+        return areaSquare;
+    }
+    
+    public void calculateAreaTotal(){
+        float surfaceArea; 
+        
+        surfaceArea = calculateAreaRectangle()+calculateAreaSquare()+calculateAreaTriangle();
+        System.out.println("surfaceArea = " + surfaceArea+" m´2");
+    }
+}
---
+++ other/PartialOne/ExerciseThree/PartialThree.java
@@ -0,0 +1,21 @@
+package coco77.partialthree;
+
+/*"3. An architect has a list of lots: 
+the data of the lots is as follows: figure type
+      (TRIANGLE, RECTANGLE, SQUARE),
+the base and the height. The total surface area of the lots is needed."*/
+public class PartialThree {
+
+    public static void main(String[] args) {
+        float baseTriangle = 5;
+        float heightTriangle = 7;
+        
+        float baseRectangle = 7;
+        float heightRectangle = 7;
+        
+        float sideSquare = 7;
+        
+        Area area = new Area( baseTriangle,  heightTriangle,  baseRectangle,  heightRectangle, sideSquare);
+        area.calculateAreaTotal();
+    }
+}
