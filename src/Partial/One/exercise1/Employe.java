711ddedca67557295e25c84a5d9fe00f81fa31a2
pintofelipe <afpintou@ufpso.edu.co>
2023-09-22 13:35:35 -0500

    update exercise 1 the partial1

---
+++ src/Partial/One/exercise1/Employe.java
@@ -0,0 +1,55 @@
+
+package coco77.exercise1;
+
+import javax.lang.model.element.Element;
+
+
+public class Employe {
+    private String document;
+    private String name;
+    private double dayWorked;
+    
+    public static final double dayWorkedPay = 38.667;
+    
+    public static  double salaryToPay;
+    
+    public Employe(String document, String name, int dayWorked) {
+        this.document = document;
+        this.name = name;
+        this.dayWorked = dayWorked;
+    }
+
+    public String getDocument() {
+        return this.document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDayWorked() {
+        return this.dayWorked*Employe.dayWorkedPay;
+    }
+
+    public void setDayWorked(int dayWorked) {
+        this.dayWorked = dayWorked;
+    }
+    
+    
+    public static void calculateSalaryToPay(Employe[] employe){
+        for(Employe e : employe) {   
+            System.out.println("The salary to pay a employe "+e.getName()+" es de: "+e.getDayWorked()+" $");
+        }   
+    }
+    
+    
+    
+}
