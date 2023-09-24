2c863061addcc4293e6abaee8f8ba3e757bb486e
pintofelipe <afpintou@ufpso.edu.co>
2023-09-24 11:18:55 -0500

    add exercise 7

---
+++ src/Partial/One/exercise7/Exercise7.java
@@ -0,0 +1,21 @@
+package coco77.exercise7;
+
+/**
+ * "7. You must create a list of products with the following data: the name of
+ * the product, the IVA and the base price. Now you must calculate the real sale
+ * price and display the total sales for the day on the console."
+ */
+public class Exercise7 {
+
+    public static void main(String[] args) {
+
+        Producto producto[] = new Producto[4];
+
+        producto[0] = new Producto("aceite", 19, 30000);
+        producto[1] = new Producto("Atún", 19, 30000);
+        producto[2] = new Producto("rice",19,30000);
+        producto[3] = new Producto("papas", 19, 30000);
+    
+        Producto.calculatePriceTotal(producto);
+    }
+}
---
+++ src/Partial/One/exercise7/Producto.java
@@ -0,0 +1,55 @@
+package coco77.exercise7;
+public class Producto {
+ 
+    private String nameProduct;
+    private double iva;
+    private double basePrice;
+    public static double salesTotal = 0;
+    public static double total;
+    
+    
+    public Producto(String nameProduct, double iva, double basePrice) {
+        this.nameProduct = nameProduct;
+        this.iva = iva;
+        this.basePrice = basePrice;
+    }
+
+    public Producto() {
+    }
+
+    public String getNameProduct() {
+        return nameProduct;
+    }
+
+    public void setNameProduct(String nameProduct) {
+        this.nameProduct = nameProduct;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(double basePrice) {
+        this.basePrice = basePrice;
+    }
+    
+    public static void calculatePriceTotal(Producto[] producto){
+        
+        for (Producto p : producto) {
+          total = ((p.getIva()*p.getBasePrice())/100)+p.getBasePrice();
+          salesTotal +=total; 
+            System.out.println("the price the producu "+p.nameProduct+" is the "+total);
+        }
+        
+        System.out.println("The total sales is the: "+salesTotal);
+    }
+    
+}
