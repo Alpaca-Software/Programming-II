b5a52170ec2286ab287cc156a317f8ab386aefa3
pintofelipe <afpintou@ufpso.edu.co>
2023-09-27 08:54:43 -0500

    add Exercise 12 parial 1

---
+++ src/Partial/One/Exercise13/Exercise13.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.exercise13;
+
+
+public class Exercise13 {
+
+    public static void main(String[] args) {
+        
+        Month month = new Month("september");
+        
+       month.calculateEndOfYear();
+    }
+}
