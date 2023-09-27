23d4014c33c6523dbadf542c05145766c871120c
pintofelipe <afpintou@ufpso.edu.co>
2023-09-26 23:37:12 -0500

    add exercise 12

---
+++ src/Partial/One/Exercise12/Digit.java
@@ -0,0 +1,33 @@
+
+package coco77.exercise12;
+
+public class Digit {
+
+    int digit;
+    String language;
+    final static String DIGITS_SPANISH[] = {"cero", "uno", "dos", "tres", "cuatro", "cinco", "seix", "siete", "ocho", "nueve"};
+    final static String DIGITS_ENGLISH[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    public Digit(int digit, String lenguage) {
+        this.digit = digit;
+        this.language = lenguage;
+    }
+
+    public boolean isSpanish() {
+        return this.language.equalsIgnoreCase("español");
+
+    }
+
+    public Digit(int digit) {
+        this.digit = digit;
+    }
+
+    public void showWord() {
+        if (isSpanish()) {
+            System.out.println("the digit en spanish is: " + Digit.DIGITS_SPANISH[digit]);
+
+        } else {
+            System.out.println("the digit en English is: " + Digit.DIGITS_ENGLISH[digit]);
+        }
+    }
+}
---
+++ src/Partial/One/Exercise12/Exercise12.java
@@ -0,0 +1,15 @@
+
+
+package coco77.exercise12;
+public class Exercise12 {
+
+    public static void main(String[] args) {
+        int digit1 = 4;
+        String language = "ingles";
+        
+        Digit digit = new Digit(digit1,language);
+        
+        digit.showWord();
+    }
+}
+
