d0729bd1217ac406dab43a150910c87f23bb0d32
alizarazot <alizarazot@ufpso.edu.co>
2023-10-13 09:39:57 -0500

    Add interactive message for run files

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,18 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package main;
 
-/**
- * @author SCIS
- */
 public class Main {
-
-  /**
-   * @param args the command line arguments
-   */
-  public static void main(String[] args) {
-    // TODO code application logic here
-  }
+    public static void main(String[] args) {
+        System.out.println("Ejecuta a nivle de archivo (Run File) o presiona Shift+F6");
+    }
 }
