9666c86163ef2a5c60fcd89c2333ec5c2891f044
alizarazot <alizarazot@ufpso.edu.co>
2023-10-13 09:46:22 -0500

    Add inheritance example

---
+++ src/Inheritance/Example/Hospital/Nurse.java
@@ -0,0 +1,30 @@
+package Inheritance.Example.Hospital;
+
+public class Nurse extends Person {
+    
+    private String code;
+    private String block;
+    
+    public Nurse(String document, String name1, String name2, String surname1, String surname2, String birthDay, String code, String block) {
+        super(document, name1, name2, surname1, surname2, birthDay);
+        
+        this.setCode(code);
+        this.setBlock(block);
+    }
+    
+    public String getBlock() {
+        return block;
+    }
+    
+    public void setBlock(String block) {
+        this.block = block;
+    }
+    
+    public String getCode() {
+        return code;
+    }
+    
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
