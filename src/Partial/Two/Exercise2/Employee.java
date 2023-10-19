08612c2e5ba07c85c76636fc1763ecda98f972d7
pintofelipe <afpintou@ufpso.edu.co>
2023-10-18 21:24:25 -0500

    add Exercise 2 partial 2

---
+++ src/Partial/Two/Exercise2/Employee.java
@@ -0,0 +1,68 @@
+
+package coco77.exercise2partial2;
+
+
+public class Employee {
+    
+    protected String nameEmploye;
+    protected float dayWorked;
+    public static final float PAYDAY = 40000; 
+    public static final float SMMLV = 1160000;
+    public static final float BONUS = 140000;
+
+    public Employee(String nameEmploye, int dayWorked) {
+        this.setNameEmploye(nameEmploye);
+        this.setDayWorked(dayWorked);
+    }
+
+    public String getNameEmploye() {
+        return nameEmploye;
+    }
+
+    public void setNameEmploye(String nameEmploye) {
+        this.nameEmploye = nameEmploye;
+    }
+
+    public float getDayWorked() {
+        return dayWorked;
+    }
+
+    public void setDayWorked(float dayWorked) {
+        this.dayWorked = dayWorked;
+    }
+    
+   public static void calculateSalaryToPay(Employee[] employes){
+       
+       for (Employee e : employes) {
+           
+          e.setDayWorked(e.dayWorked*PAYDAY);
+       }
+       
+     for (Employee e : employes) {
+           System.out.println(e.nameEmploye+" el salario del empleado es de:  "+e.dayWorked);
+       }
+       
+       System.out.println("###############################");
+       bonificationEmploye(employes);
+   }
+   
+   
+   public static void bonificationEmploye(Employee[] salarys){
+       
+       
+       for (Employee e : salarys) {
+           
+           if(e.dayWorked < SMMLV){
+               e.dayWorked += BONUS;
+           }
+            
+       }
+       
+       for (Employee e : salarys) {
+           System.out.println(e.nameEmploye+" el salario del empleado es de:  "+e.dayWorked);
+       }
+       
+       
+   }
+    
+}
---
+++ src/Partial/Two/Exercise2/Exercise2Partial2.java
@@ -0,0 +1,26 @@
+
+package coco77.exercise2partial2;
+
+/*"2. Del ejercicio anterior se deberá enviar la lista a un nuevo método y añadir una bonificación al salario si el trabajador gana menos de un MÍNIMO.
+     Mostrar la nueva lista por consola."*/
+public class Exercise2Partial2 {
+
+    public static void main(String[] args) {
+        
+        Employee employes[] = new Employee[5];
+        
+        employes[0] = new Employee("felipe",18);
+        employes[1] = new Employee("Luis",1);
+        employes[2] = new Employee("Eduardo",0);
+        employes[3] = new Employee("Coco77",22);
+        employes[4] = new Employee("Andrey",72);
+    
+        Employee.calculateSalaryToPay(employes);
+       
+        
+        
+        
+        
+        
+    }
+}
