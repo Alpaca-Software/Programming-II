7dc4b081be932d21ba17e88f07e5a0f245848075
pintofelipe <afpintou@ufpso.edu.co>
2023-10-26 22:53:23 -0500

    add Exercise 4 partial 2

---
+++ src/Partial/Two/Exercise4/main.java
@@ -0,0 +1,24 @@
+
+package Partial.Two.Exercise4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*"4. An architect has a list of lots: the data of the lots are the following: type of figure (RECTANGLE and SQUARE), the base and the height.
+    The sum of the areas of the lots is needed."*/
+public class main {
+    public static void main(String[] args) {
+    
+        
+        List<Area> areas = new ArrayList<>();
+        
+        areas.add(new Area(10f,34f));
+        areas.add(new Area(60f,70f));
+        areas.add(new Area(70f,40f));
+        areas.add(new Area(10f,34f));
+        
+        float sum = 0;
+        sum = Area.calculateSumOfAreas((ArrayList<Area>)areas);
+        System.out.println(sum);
+    }
+}
