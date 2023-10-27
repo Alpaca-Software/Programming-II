f3579af798b667ba25e16976aeb943dd47f8b6f8
alizarazot <alizarazot@ufpso.edu.co>
2023-10-27 09:24:25 -0500

    Add a review exercise (the first)

---
+++ src/Review/Exercise1/Main.java
@@ -0,0 +1,35 @@
+package Review.Exercise1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    final static double SALARY = 45_000;
+    final static double MINIMUM_SALARY = 1_160_000;
+    final static double TRANSPORT_BONUS = 50_000;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Days workded: ");
+        double days = sc.nextDouble();
+
+        System.out.printf("Salary of %s is $%.0f\n", name, addTransportBonus(getSalary(days)));
+    }
+
+    public static double getSalary(double days) {
+        return days * Main.SALARY;
+    }
+
+    public static double addTransportBonus(double salary) {
+        if (salary < Main.MINIMUM_SALARY / 2) {
+            return salary + Main.TRANSPORT_BONUS;
+        }
+
+        return salary;
+    }
+}
