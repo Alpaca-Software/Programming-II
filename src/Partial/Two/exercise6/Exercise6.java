b72f103834e3770e5b67486e040ab54950379fe0
pintofelipe <afpintou@ufpso.edu.co>
2023-10-31 10:01:38 -0500

    add Exercise 6 partial 2

---
+++ src/Partial/Two/exercise6/Exercise6.java
@@ -0,0 +1,30 @@
+
+package coco77.exercise6;
+import java.util.ArrayList;
+import java.util.List;
+/* @author Coco77
+
+6. Add the following data to the object of the previous list, the score of the first and second partial, you want to know the 70 percent.
+ */
+import java.util.List;
+public class Exercise6 {
+
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        
+        students.add(new Student("felipe",5.0f,4.8f,5.0f));
+        students.add(new Student("ponto",3.0f,1.8f,4.0f));
+        students.add(new Student("Johana",5.0f,4.8f,5.0f));
+        students.add(new Student("pinto",5.0f,5.0f,5.0f));
+        
+        List notas = Student.calculate70Persent(students);
+        
+        int c = notas.size();
+        
+        for (int i = 0; i < c; i++) {
+            
+        }
+     
+        
+    }
+}
---
+++ src/Partial/Two/exercise6/Student.java
@@ -0,0 +1,71 @@
+
+package coco77.exercise6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    private String name;
+    private float quizzes;
+    private float partialOne;
+    private float partialTwo;
+
+    public Student(String name, float quizzes, float partialOne, float partialTwo) {
+        this.setName(name);
+        this.setQuizzes(quizzes);
+        this.setPartialOne(partialOne);
+        this.setPartialTwo(partialTwo);
+    }
+
+    public float getPartialTwo() {
+        return partialTwo;
+    }
+
+    public void setPartialTwo(float partialTwo) {
+        this.partialTwo = partialTwo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getQuizzes() {
+        return quizzes;
+    }
+
+    public void setQuizzes(float quizzes) {
+        this.quizzes = quizzes;
+    }
+
+    public float getPartialOne() {
+        return partialOne;
+    }
+
+    public void setPartialOne(float partialOne) {
+        this.partialOne = partialOne;
+    }
+    
+    public static List calculate70Persent(List<Student> students){
+        List<Float> notes = new ArrayList<>();
+        
+        float noteStudent =0.0f;
+        
+        for (Student e : students) {
+            noteStudent =0.0f;
+            
+            noteStudent += e.quizzes+e.partialOne+e.partialTwo;
+            
+            noteStudent = (noteStudent/3)*.7f;
+            
+            
+            notes.add(noteStudent);
+        }
+        
+        return notes;
+    }
+    
+}
