f255e37f2ecadc6142b59eb7081956c598d61490
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2023-11-01 15:06:50 -0500

    Fix inusual name

---
+++ src/oop/exercise1A/Main.java
@@ -0,0 +1,20 @@
+package oop.exercise1A;
+
+/*1. Solicite una palabra mediante el teclado y muéstrela en la pantalla letra por letra, usando [ toCharArray() ]
+ 1. Request a word by keyboard and display it on the screen letter by letter, using [ toCharArray() ]
+*/
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter a word: ");
+    String word = sc.nextLine();
+
+    Word character = new Word(word);
+
+    character.veryWord();
+  }
+}
---
+++ src/oop/exercise4/Random.java
@@ -0,0 +1,30 @@
+package oop.exercise4;
+
+public class Random {
+  public int number;
+
+  public Random(int number) {
+    this.number = number;
+  }
+
+  public void veryRandom() {
+    int c = 0;
+
+    if (number > 0 && number <= 20) {
+
+      for (int i = 0; i < number; i++) {
+        int numberOne = (int) (Math.random() * number + 1);
+
+        if (numberOne == number) {
+          c++;
+        }
+        System.out.println(numberOne);
+      }
+
+    } else {
+      System.out.println("The number must be greater than 0 and less than or equal to 20");
+    }
+
+    System.out.println("The number fue encontrado " + c + " veces");
+  }
+}
