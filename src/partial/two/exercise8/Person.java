35284f952251427c7b61816dc49db9555d58635c
pintofelipe <afpintou@ufpso.edu.co>
2023-11-02 21:21:57 -0500

    add Exercise 8 partial 2

---
+++ src/partial/two/exercise8/Person.java
@@ -0,0 +1,55 @@
+package coco77.exercise8;
+
+import java.util.ArrayList;
+
+/* @author pinto*/
+public class Person {
+    private String name;
+    private String document;
+    private int age;
+    public static float ageAvererage = 0;
+
+    public Person(String name, String document, int age) {
+        this.name = name;
+        this.document = document;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+    
+    public static float averageAge(ArrayList<Person> people){
+        
+        int sizeList = people.size();
+        
+        for (Person p : people) {
+            Person.ageAvererage += p.age;
+        }
+        
+        Person.ageAvererage /= sizeList;
+        
+        return Person.ageAvererage;
+    }
+    
+}
