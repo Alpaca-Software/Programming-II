b377e876f53641331267307cb0277d7c07dc469f
pintofelipe <afpintou@ufpso.edu.co>
2023-11-02 21:22:14 -0500

    add Exercise 9 partial 2

---
+++ src/partial/two/exercise9/Exercise9.java
@@ -0,0 +1,23 @@
+
+package coco77.exercise9;
+
+import java.util.ArrayList;
+
+/* @author pinto
+"9. Create a list with the following data: an amount of money and the type of currency 
+(dollars and euros), through a method to convert the values into Colombian pesos, 
+    and display the total amount of money collected in Colombian pesos on the console. Use two methods."
+*/
+public class Exercise9 {
+
+    public static void main(String[] args) {
+        ArrayList<Currency> currencys = new ArrayList<>();
+        
+        currencys.add(new Currency(1000,"dollars"));
+        currencys.add(new Currency(1000,"euros"));
+        currencys.add(new Currency(5000,"dollars"));
+        currencys.add(new Currency(20000,"euros"));
+        
+        Currency.showCurrencyConvert(currencys);
+    }
+}
