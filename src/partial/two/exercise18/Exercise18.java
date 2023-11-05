fd481254cc59439c11c5d10ed0a1f80518c491a4
pintofelipe <afpintou@ufpso.edu.co>
2023-11-05 11:30:20 -0500

    add Exercise 18 partial 2

---
+++ src/partial/two/exercise18/Exercise18.java
@@ -0,0 +1,20 @@
+
+package coco77.exercise18;
+/*
+18. Requests the name of a month by keyboard 
+and shows by console how many months are 
+left until December.
+
+18. Solicita el nombre de un mes por teclado y muestra por consola cuantos meses faltan para diciembre.
+*/
+public class Exercise18 {
+
+    public static void main(String[] args) {
+        
+     
+        Month month1 = new Month("january");
+        
+        month1.calculateEndOfYear();
+        
+    }
+}
---
+++ src/partial/two/exercise18/Month.java
@@ -0,0 +1,53 @@
+package coco77.exercise18;
+
+public class Month {
+
+    private String mosth;
+
+    public static final String MONTHS[] = {
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "Agust",
+        "September",
+        "Octuber",
+        "November",
+        "December"};
+
+    public Month(String mosth) {
+        this.setMosth(mosth);
+    }
+
+    public String getMosth() {
+        return mosth;
+    }
+
+    public void setMosth(String mosth) {
+        this.mosth = mosth;
+    }
+
+    public void calculateEndOfYear() {
+        int size = MONTHS.length;
+        int monthIndex = -1;
+
+        for (int i = 0; i < size; i++) {
+            if (MONTHS[i].equalsIgnoreCase(mosth)) {
+                monthIndex = i;
+                break;
+            }
+        }
+        
+        String outputMessage = (monthIndex != -1)
+                ? "missingMonths = "+((MONTHS.length)-(monthIndex+1))
+                : "el mes especificado no se encontro";
+        
+        System.out.println(outputMessage);
+
+
+    }
+
+}
