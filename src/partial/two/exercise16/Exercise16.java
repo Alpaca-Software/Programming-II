5740b88a685f9ed596ffb6cc6c4e9a2c3c869b97
pintofelipe <afpintou@ufpso.edu.co>
2023-11-05 05:01:01 +0100

    add Exercise 16 partial 2

---
+++ src/partial/two/exercise16/Exercise16.java
@@ -0,0 +1,34 @@
+package coco77.exercise16;
+
+import java.util.ArrayList;
+
+/*
+"16. From the previous exercise, create a method that, 
+upon receiving the list of numbers, performs a sum and returns the value to the main method, that value will verify 
+      if it is an even number."
+ */
+public class Exercise16 {
+
+    public static void main(String[] args) {
+        ArrayList<Number> numbers = new ArrayList<>();
+        
+        numbers.add(new Number(77));
+        numbers.add(new Number(7));
+        numbers.add(new Number(0));
+        numbers.add(new Number(2));
+        numbers.add(new Number(80));
+        numbers.add(new Number(3));
+        numbers.add(new Number(5));
+        
+        double sumNumbers = Number.calculateSumTotal(numbers);
+        
+        System.out.println("sumNumbers = " + sumNumbers);
+        
+        
+        if(sumNumbers%2 !=0){
+            System.out.println("el numero "+sumNumbers+" es impar");
+        }else{
+            System.out.println("el numero "+sumNumbers+" es par");
+        }
+    }
+}
---
+++ src/partial/two/exercise16/Number.java
@@ -0,0 +1,34 @@
+
+package coco77.exercise16;
+
+/*coco77
+ */
+import java.util.ArrayList;
+public class Number {
+    
+    private double number;
+    public static double totalSum=0;
+
+    public Number(double number) {
+        this.number = number;
+    }
+
+    public double getNumber() {
+        return number;
+    }
+
+    public void setNumber(double number) {
+        this.number = number;
+    }
+    
+    public static double calculateSumTotal(ArrayList<Number> numbers){
+        
+        for (Number n : numbers) {
+            Number.totalSum += n.number;
+        }
+        
+        return Number.totalSum;
+    }
+    
+    
+}
