c3ac43d82f8e4efd33752af1cf93492c7bb47677
pintofelipe <afpintou@ufpso.edu.co>
2023-11-05 04:35:41 +0100

    add Exercise 14 partial 2

---
+++ src/partial/two/exercise14/Exercise14.java
@@ -0,0 +1,25 @@
+
+package coco77.exercise14;
+
+import java.util.ArrayList;
+
+/*@author pinto77*/
+public class Exercise14 {
+
+    public static void main(String[] args) {
+        ArrayList<Number> numbers = new ArrayList<>();
+        
+        numbers.add(new Number(77));
+        numbers.add(new Number(70));
+        numbers.add(new Number(45));
+        numbers.add(new Number(17));
+        numbers.add(new Number(-3+4));
+        
+        double legalNumber = Number.calculateMaxNumber(numbers);
+        double minorNumber = Number.calculateMinNumber(numbers);
+        
+        
+        System.out.println("legal: "+legalNumber);
+        System.out.println("Minor: "+minorNumber);
+    }
+}
---
+++ src/partial/two/exercise14/Number.java
@@ -0,0 +1,55 @@
+package coco77.exercise14;
+
+import java.util.ArrayList;
+
+/* @author Pinto77*/
+public class Number {
+    private double number;
+    public static Double minNumber;
+    public static double maxNumber;
+    
+    
+    public Number(double number) {
+        this.setNumber(number);
+    }
+
+    public double getNumber() {
+        return number;
+    }
+
+    public void setNumber(double number) {
+        this.number = number;
+    }
+    
+    
+    public static double calculateMinNumber(ArrayList<Number> numbers){
+
+        Number.minNumber = numbers.get(0).getNumber();
+        
+        for (Number n : numbers) {
+           if( n.number < Number.minNumber){
+               Number.minNumber = n.number;
+            }
+            
+        }
+        
+        return Number.minNumber;
+    }
+    
+    
+    public static double calculateMaxNumber(ArrayList<Number> numbers){
+        
+       Number.minNumber = numbers.get(0).getNumber();
+        
+        for (Number n: numbers) {
+            if(n.number > Number.maxNumber){
+                Number.maxNumber = n.number;
+            }
+        }
+        
+        
+       
+        return Number.maxNumber;
+    }
+    
+}
