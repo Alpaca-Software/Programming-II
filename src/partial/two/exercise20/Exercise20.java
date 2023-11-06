68ac4e0d4c02856171a5e2cc3d60acf856b2964b
pintofelipe <afpintou@ufpso.edu.co>
2023-11-06 16:55:29 -0500

    add Exercise 20 partial 2

---
+++ src/partial/two/exercise20/Exercise20.java
@@ -0,0 +1,22 @@
+package coco77.exercise20;
+
+import java.util.Scanner;
+
+/*
+20. Request a word using the keyboard, display the same word with the first and last letter capitalized.
+
+20. Solicite una palabra usando el teclado, muestre la misma palabra con la primera y la última letra en mayúscula.
+
+ */
+public class Exercise20 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String word = sc.next();
+        
+        Word word1 = new Word(word);
+        
+        word1.showWordUpperCase();
+    }
+}
---
+++ src/partial/two/exercise20/Word.java
@@ -0,0 +1,31 @@
+package coco77.exercise20;
+
+/*77*/
+public class Word {
+
+    private String word;
+
+    public Word(String word) {
+        this.setWord(word);
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public void showWordUpperCase() {
+
+        int c = this.word.length();
+        if(this.word != null && !this.word.isEmpty()){
+            this.word = this.word.substring(0, 1).toUpperCase()+ this.word.substring(1, c-1)+this.word.substring(c-1, c).toUpperCase() ;
+        }
+
+        System.out.println(this.word);
+      
+    }
+
+}
