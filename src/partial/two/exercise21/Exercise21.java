892b142c397da7afb2ea29d7878e30a32f6c85be
pintofelipe <afpintou@ufpso.edu.co>
2023-11-07 08:00:26 -0500

    add Exercise 21 partial 2

---
+++ src/partial/two/exercise21/Exercise21.java
@@ -0,0 +1,31 @@
+
+package coco77.exercise21;
+
+/*
+"21. Enseñe las vocales al sistema, guárdelas en un arreglo de la siguiente manera,
+en la primera posición del arreglo almacene un arreglo con las vocales en minúsculas 
+y en la segunda posición del array almacena un array con las vocales en mayúsculas, 
+luego solicita una letra por teclado y comprueba si es una vocal."
+
+"21. Teach the vowels to the system, store them in an array as follows, in the first
+position of the array store an array with the vowels in lowercase and in the second position 
+of the array store an array with the vowels in uppercase, then request a letter by
+keyboard and check if it is a vowel."
+ */
+import java.util.*;
+import java.util.ArrayList;
+public class Exercise21 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+      String vowel = sc.next();
+        
+        Vowel vowel1 = new Vowel(vowel);
+        
+        vowel1.showIfIsVowel();
+        
+       
+        
+    }
+}
