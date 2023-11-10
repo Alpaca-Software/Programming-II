761639ef44bcc5e009dfdf2595f2c7ae9693a783
pintofelipe <afpintou@ufpso.edu.co>
2023-11-09 19:31:02 -0500

    add Exercise 26 partial 2

---
+++ src/partial/two/exercise26/Body.java
@@ -0,0 +1,33 @@
+package coco77.exercise26;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author pinto
+ */
+public class Body {
+    private String partyBody;
+
+    public Body(String partyBody) {
+        this.setPartyBody(partyBody);
+    }
+
+    public String getPartyBody() {
+        return partyBody;
+    }
+
+    public void setPartyBody(String partyBody) {
+        this.partyBody = partyBody;
+    }
+    
+    
+    public static void showListPartysBody(ArrayList<Body> body){
+        int size = body.size();
+        
+        for (int i = size-1; i >= 0; i--) {
+            System.out.println(body.get(i).partyBody);
+        }
+    }
+    
+}
