61c719ba93466bfcfdd3e63e7759460c4c7522b4
pintofelipe <afpintou@ufpso.edu.co>
2023-11-09 22:53:59 -0500

    add Exercise 31 partial 2

---
+++ src/partial/two/exercise31/Color.java
@@ -0,0 +1,36 @@
+package coco77.exercise31;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Color {
+    private String color;
+
+    public Color(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    public static List calculateColor(ArrayList<Color> colors, String colorB){
+        
+       int size = colors.size();
+       
+        for (int i = 0; i < size; i++) {
+            if(colors.get(i).color.equalsIgnoreCase(colorB)){
+               
+                colors.set(i, new Color("purple"));
+            }
+        }
+        
+        
+        return colors;
+    }
+    
+}
